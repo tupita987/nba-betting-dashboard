4c2a1c108f9a7ae629b431ae8330dca66b6e4a64
tupita987 <hauitimai@gmail.com>
2026-01-11 04:04:58 -1000

    Update today_games.py

--- analysis/today_games.py
+++ analysis/today_games.py
@@ -6,9 +6,20 @@ def get_today_games():
     board = scoreboardv2.ScoreboardV2(game_date=today)
     df = board.get_data_frames()[0]
 
-    return df[[
-        "HOME_TEAM_ID",
-        "HOME_TEAM_NAME",
-        "VISITOR_TEAM_ID",
-        "VISITOR_TEAM_NAME"
-    ]]
+    # Colonnes possibles selon l'API
+    possible_cols = {
+        "HOME_TEAM_NAME": ["HOME_TEAM_NAME", "HOME_TEAM_ABBREVIATION"],
+        "VISITOR_TEAM_NAME": ["VISITOR_TEAM_NAME", "VISITOR_TEAM_ABBREVIATION"],
+        "HOME_TEAM_ID": ["HOME_TEAM_ID"],
+        "VISITOR_TEAM_ID": ["VISITOR_TEAM_ID"],
+    }
+
+    data = {}
+
+    for key, variants in possible_cols.items():
+        for col in variants:
+            if col in df.columns:
+                data[key] = df[col]
+                break
+
+    return data
